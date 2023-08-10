8e71442e5fd3f2c96a196fd44ad439d77092b557
Victorcardosolacerda <victor.ox@hotmail.com.br>
2023-08-10 05:28:40 -0300

    Exercicio 7

--- exercicios.js
+++ exercicios.js
@@ -1,51 +1,51 @@
 // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
 
 // EXERCÍCIO 0A
-let num1 = 2
-let num2 = 5
-function soma(num1, num2) {
+// let num1 = 2
+// let num2 = 5
+// function soma(num1, num2) {
   
-  return num1 + num2
-}
+//   return num1 + num2
+// }
 
-// EXERCÍCIO 0B
-function imprimeMensagem() {
-  // implemente sua lógica aqui
-  const mensagem = prompt('Sejam bem vindos!')
+// // EXERCÍCIO 0B
+// function imprimeMensagem() {
+//   // implemente sua lógica aqui
+//   const mensagem = prompt('Sejam bem vindos!')
 
-  console.log(mensagem)
-}
-imprimeMensagem('Sejam bem vindos!')
+//   console.log(mensagem)
+// }
+// imprimeMensagem('Sejam bem vindos!')
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
 
-function calculaAreaRetangulo(altura, largura) {
-  altura = prompt("Qual a altura do retangulo?")
-  largura = prompt("Qual a largura do retangulo?")
-  let area = altura * largura
-  console.log (area)
-}
-calculaAreaRetangulo(3, 5)
+// function calculaAreaRetangulo(altura, largura) {
+//   altura = prompt("Qual a altura do retangulo?")
+//   largura = prompt("Qual a largura do retangulo?")
+//   let area = altura * largura
+//   console.log (area)
+// }
+// calculaAreaRetangulo(3, 5)
 
-// EXERCÍCIO 02
-function imprimeIdade(anoAtual, anoDeNascimento) {
-  anoAtual = prompt("Qual o ano atual?")
-  anoDeNascimento = prompt ("Qual a sua data de nascimento?")
-  let idade = anoAtual - anoDeNascimento
-  console.log(idade)
+// // EXERCÍCIO 02
+// function imprimeIdade(anoAtual, anoDeNascimento) {
+//   anoAtual = prompt("Qual o ano atual?")
+//   anoDeNascimento = prompt ("Qual a sua data de nascimento?")
+//   let idade = anoAtual - anoDeNascimento
+//   console.log(idade)
 
-}
-imprimeIdade()
+// }
+// imprimeIdade()
 
-// EXERCÍCIO 03
-function calculaIMC(peso, altura) {
-  peso = prompt("Qual seu peso?")
-  altura = prompt("Qual sua altura?")
-  return peso/(altura*altura)
+// // EXERCÍCIO 03
+// function calculaIMC(peso, altura) {
+//   peso = prompt("Qual seu peso?")
+//   altura = prompt("Qual sua altura?")
+//   return peso/(altura*altura)
 
-}
-calculaIMC()
+// }
+// calculaIMC()
 
 // EXERCÍCIO 04
 //function imprimeInformacoesUsuario(nome, idade, email) {
@@ -57,29 +57,33 @@ calculaIMC()
 //}
 //imprimeInformacoesUsuario()
 
-// EXERCÍCIO 05
+//EXERCÍCIO 05
 // function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
 //   cor1 = prompt("Qual a sua cor favorita?")
 //   cor2 = prompt("Qual sua segunda cor favorita?")
 //   cor3 = prompt("Qual sua terceira cor favorita?")
 //  console.log()
+//}
+//imprimeTresCoresFavoritas()
+
+// // EXERCÍCIO 06
+// function retornaStringEmMaiuscula(string) {
+//   string = prompt("Digite uma frase")
+//   const fraseMaiuscula = string.toUpperCase()
+//   return fraseMaiuscula
+
 // }
-// imprimeTresCoresFavoritas()
+// retornaStringEmMaiuscula()
 
-// EXERCÍCIO 06
-function retornaStringEmMaiuscula(string) {
-  string = prompt("Digite uma frase")
-  const fraseMaiuscula = string.toUpperCase()
-  return fraseMaiuscula
-
-}
-retornaStringEmMaiuscula()
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+  custo = prompt("Qual valor das despesas gastas?")
+  valorIngresso = prompt("Qual valor será vendido os ingressos?")
+  let necessarioVender = custo/valorIngresso
+  return necessarioVender
 
 }
-
+calculaIngressosEspetaculo()
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
